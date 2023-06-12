a41af693b154ddc10860daceab212d1e8860a609
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2023-06-12 18:56:19 +0300

    12/6/2023

--- src/test/java/LMSProject/C01_Lms.java
+++ src/test/java/LMSProject/C01_Lms.java
@@ -1,20 +1,20 @@
 package LMSProject;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.time.Duration;
 
 public class C01_Lms {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+    public static void main(String[] args) throws InterruptedException, AWTException {
+        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
 
         //TechproEducation sayfasina git
         driver.get("https://techproeducation.com");
@@ -37,7 +37,7 @@ public class C01_Lms {
 
         //Username ve Password bilgilerini gir
         WebElement username = driver.findElement(By.cssSelector("#username"));
-        username.sendKeys("mehmetfatihh57gmailcom" , Keys.ENTER , Keys.TAB , "YJArAXHAQ-27" , Keys.ENTER);
+        username.sendKeys("mehmetfatihh57gmailcom", Keys.ENTER, Keys.TAB, "YJArAXHAQ-27", Keys.ENTER);
         Thread.sleep(2000);
 
         //Automation Engineer bolumune gir
@@ -49,12 +49,25 @@ public class C01_Lms {
         WebElement goToZoom = driver.findElement(By.xpath("//a[@class='banner-btn']"));
         goToZoom.click();
         Thread.sleep(2000);
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+        Thread.sleep(3000);
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+        Robot robot = new Robot();
 
-       //WebElement acceptCookies = driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
-       //acceptCookies.click();
-       //Thread.sleep(2000);
+        // ENTER tuşunu simüle ederek alert'i kabul edebiliriz
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
 
-       //WebElement launchMeeting = driver.findElement(By.xpath("//div[@class='mbTuDeF1']"));
-       //launchMeeting.click();
+        WebElement acceptCookies = driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
+        acceptCookies.click();
+        Thread.sleep(2000);
+
+        WebElement launchMeeting = driver.findElement(By.xpath("//div[@class='mbTuDeF1']"));
+        launchMeeting.click();
     }
 }
